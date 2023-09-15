d2ed11140bdc00998082469e67c397332c557fd2
prc-gllg <143255141+prc-gllg@users.noreply.github.com>
2023-09-15 10:43:09 +0800

    Initial Commit

---
+++ MovieMania/MovieManiaApp.swift
@@ -0,0 +1,20 @@
+//
+//  MovieManiaApp.swift
+//  MovieMania
+//
+//  Created by Pierce Gallego on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieManiaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
